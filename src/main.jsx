5f86a17336d42d43d17cde89a099d54070f952be
Amit_Sahoo <136674440+AmitSahoo7@users.noreply.github.com>
2025-07-09 21:39:43 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,12 @@ import AOS from 'aos';
 
 AOS.init(); // Initialize AOS
 
-export const server = "https://skillnest-backend.onrender.com";
+export const server = import.meta.env.VITE_SERVER;
+
+if (!server) {
+  throw new Error("VITE_SERVER is not defined. Check your environment variables.");
+}
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
